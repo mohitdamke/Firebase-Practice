db30f097ae68b41b6ec8d2360b506eccbe6e8a97
raees <mohitdamke1@gmail.com>
2024-06-29 15:55:00 +0530

    Bottom NavBar startup file

--- app/src/main/java/com/example/firebasepractice/ItemState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.firebasepractice
-
-import com.example.firebasepractice.firReDb.RealtimeModelResponse
-
-data class ItemState(
-    val item : List<RealtimeModelResponse> = emptyList(),
-    val isLoading : Boolean = false,
-    val error : String = ""
-)
--- app/src/main/java/com/example/firebasepractice/MainActivity.kt
+++ app/src/main/java/com/example/firebasepractice/MainActivity.kt
@@ -4,40 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import dagger.hilt.android.AndroidEntryPoint
+import androidx.navigation.compose.rememberNavController
 
-@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val isInsert = remember {
-                mutableStateOf(false)
-            }
-            Scaffold(floatingActionButton = {
-                FloatingActionButton(onClick = { isInsert.value = true }) {
-                    Icon(
-                        imageVector = Icons.Default.Add,
-                        contentDescription = "",
-                        modifier = Modifier.size(30.dp)
-                    )
+            Surface(modifier = Modifier.fillMaxSize()) {
 
-                }
-            }) {
-                RealtimeScreen(isInsert, it)
+                val navController = rememberNavController()
+                MainScreen(navController)
             }
         }
     }
